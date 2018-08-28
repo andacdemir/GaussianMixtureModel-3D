664377441187b61c19145ec9fbb0ec32b46f0085
Andac Demir <ademir@ece.neu.edu>
2018-08-28 15:55:57 -0400

    edit classification utilities

--- classification_utilities.py
+++ classification_utilities.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-def display_cm(cm, labels, hide_zeros=False,
-                             display_metrics=False):
-    """Display confusion matrix with labels, along with
-       metrics such as Recall, Precision and F1 score.
-       Based on Zach Guo's print_cm gist at
-       https://gist.github.com/zachguo/10296432
-    """
-
+"""
+    Display confusion matrix with labels, along with
+    metrics such as Recall, Precision and F1 score.
+    Based on Zach Guo's print_cm gist at
+    https://gist.github.com/zachguo/10296432
+"""
+def display_cm(cm, labels, hide_zeros=False, display_metrics=False):
     precision = np.diagonal(cm)/cm.sum(axis=0).astype('float')
     recall = np.diagonal(cm)/cm.sum(axis=1).astype('float')
     F1 = 2 * (precision * recall) / (precision + recall)
@@ -28,7 +27,7 @@ def display_cm(cm, labels, hide_zeros=False,
     for label in labels: 
         print("%{0}s".format(columnwidth) % label,)
     print("%{0}s".format(columnwidth) % 'Total')
-    print
+    print()
     print("    " + " True")
     # Print rows
     for i, label1 in enumerate(labels):
@@ -40,43 +39,43 @@ def display_cm(cm, labels, hide_zeros=False,
             print(cell,)
         print("%{0}d".format(columnwidth) % sum(cm[i,:]))
         
-        print
+        print()
         
     if display_metrics:
-        print
+        print()
         print("Precision",)
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % precision[j]
             print(cell,)
+        
         print("%{0}.2f".format(columnwidth) % total_precision)
-        print
+        print()
         print("   Recall",)
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % recall[j]
             print(cell,)
+        
         print("%{0}.2f".format(columnwidth) % total_recall)
-        print
+        print()
         print("       F1",)
         for j in range(len(labels)):
             cell = "%{0}.2f".format(columnwidth) % F1[j]
             print(cell,)
+        
         print("%{0}.2f".format(columnwidth) % total_F1)
-        print
+        print()
     
-                  
-def display_adj_cm(
-        cm, labels, adjacent_facies, hide_zeros=False, 
-        display_metrics=False):
-    """This function displays a confusion matrix that counts 
-       adjacent facies as correct.
-    """
+"""
+    This function displays a confusion matrix that counts 
+    adjacent facies as correct.
+"""                  
+def display_adj_cm(cm, labels, adjacent_facies, hide_zeros=False, 
+                   display_metrics=False):
     adj_cm = np.copy(cm)
-    
     for i in np.arange(0,cm.shape[0]):
         for j in adjacent_facies[i]:
             adj_cm[i][i] += adj_cm[i][j]
             adj_cm[i][j] = 0.0
         
-    display_cm(adj_cm, labels, hide_zeros, 
-                             display_metrics)
+    display_cm(adj_cm, labels, hide_zeros, display_metrics)
         
